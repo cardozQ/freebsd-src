4fbaca7bc6fcd4f4f24cc1b51afebc191041f810
Abhinav Chavali <abhinavchavali12@gmail.com>
2025-07-27 13:18:22 -0400

    bf

--- sys/amd64/vmm/intel/vmx.c
+++ sys/amd64/vmm/intel/vmx.c
@@ -2602,6 +2602,7 @@ vmx_exit_process(struct vmx *vmx, struct vmx_vcpu *vcpu, struct vm_exit *vmexit)
 		vmx_clear_int_window_exiting(vcpu);
 		return (1);
 	case EXIT_REASON_EXT_INTR:
+        printf("Hi I got here\n");
 		/*
 		 * External interrupts serve only to cause VM exits and allow
 		 * the host interrupt handler to run.
