e56b319938e4d5d09aa6a2645ff647b00a6ff025
Abhinav Chavali <abhinavchavali12@gmail.com>
2025-09-08 17:16:56 -0400

    bf

--- lib/libvmmapi/amd64/vmmapi_machdep.c
+++ lib/libvmmapi/amd64/vmmapi_machdep.c
@@ -828,3 +828,16 @@ vcpu_reset(struct vcpu *vcpu)
 done:
 	return (error);
 }
+
+void
+vm_inject_fault(struct vcpu *vcpu, int vector, int errcode_valid,
+    int errcode)
+{
+	int error, restart_instruction;
+
+	restart_instruction = 1;
+
+	error = vm_inject_exception(vcpu, vector, errcode_valid, errcode,
+	    restart_instruction);
+	assert(error == 0);
+}
